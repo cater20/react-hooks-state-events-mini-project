4c363517c7d88a2b61957e7524272067d5fc4c14
chrisphine <onyangoketta@gmail.com>
2022-07-21 17:45:58 +0300

    updated categories to task

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+const[isSelected,setIsSelected]=useState(false)
+
+
+const handleClick = (id) => {
+     
+  categories.map((category, index) => {
+      if(index === id){
+        setIsSelected(!isSelected)
+      }
+  })
+  
+}
+
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-     
+      {categories.map((category, index) => {
+        return <button  key={index} className={isSelected? "selected" : ""}>{category}</button>
+      })}
     </div>
   );
 }
